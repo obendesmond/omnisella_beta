a636885187399a750463be9991a1339d74b0270f
obendesmond <obendesmond2@gmail.com>
2022-05-10 22:36:45 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const App = () => {
+  // var, let, const
+  const person = {
+    name: {
+      firstname: "Misadah",
+      lastname: "Desmond",
+    },
+    course: "React Course with Desmond",
+    height: 25,
+    size: 40,
+    school: "Government High School",
+  };
+
+  person.weight = "400kg";
+  delete person.height; // delete person["height"]
+
+  //   loop through an object using for in
+  for (let x in person) {
+    console.log(person[x]);
+  }
+
+  //   person.name or person[name]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello World</h1>
     </div>
   );
-}
+};
 
 export default App;
