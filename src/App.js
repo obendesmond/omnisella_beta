a8030fae46eb15aa2b22c44ce1f0df6765ca8788
obendesmond <obendesmond2@gmail.com>
2022-05-13 00:28:11 +0100

    init and finish form

--- src/App.js
+++ src/App.js
@@ -1,31 +1,14 @@
 import React from "react";
+import { HiMenu } from "react-icons/hi";
+import "./App.css";
 
 const App = () => {
-  // var, let, const
-  const person = {
-    name: {
-      firstname: "Misadah",
-      lastname: "Desmond",
-    },
-    course: "React Course with Desmond",
-    height: 25,
-    size: 40,
-    school: "Government High School",
-  };
-
-  person.weight = "400kg";
-  delete person.height; // delete person["height"]
-
-  //   loop through an object using for in
-  for (let x in person) {
-    console.log(person[x]);
-  }
-
-  //   person.name or person[name]
-
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="page">
+      <form type="submit" className="formContainer">
+        <HiMenu className="menuIcon" />
+        <input placeholder="Enter todo list" type="text" />
+      </form>
     </div>
   );
 };
