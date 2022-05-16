ce7672733044b5eb0cab923f3deb27318f55fa20
obendesmond <obendesmond2@gmail.com>
2022-05-16 22:38:57 +0100

    implement addTodo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { HiMenu } from "react-icons/hi";
+import Card from "./Card";
 import "./App.css";
 
 const App = () => {
+  const [todo, setTodo] = useState("");
+  const [list, setList] = useState([]);
+
+  const handleOnChange = value => {
+    setTodo(value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setList([...list, todo]);
+  };
+
   return (
     <div className="page">
-      <form type="submit" className="formContainer">
+      <form
+        type="submit"
+        onSubmit={e => handleSubmit(e)}
+        className="formContainer"
+      >
         <HiMenu className="menuIcon" />
-        <input placeholder="Enter todo list" type="text" />
+        <input
+          value={todo}
+          onChange={e => handleOnChange(e.target.value)}
+          placeholder="Enter todo list"
+          type="text"
+        />
       </form>
+      {list.map((list, i) => (
+        <p key={i}>{list}</p>
+      ))}
     </div>
   );
 };
